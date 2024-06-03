0dfd3ba82e9a44169065099bd42a6972f1e6df8c
RogerMK17 <139058610+RogerMK17@users.noreply.github.com>
2024-06-03 09:57:56 +0500

    new

---
+++ src/main/java/packages/project/Vehicle/VehicleClass.java
@@ -0,0 +1,362 @@
+package packages.project.Vehicle;
+
+public class VehicleClass {
+
+    public void doNothing() {
+        for (int i = 0; i < 10; i++) {
+            int result = i * i;
+            result = result;
+        }
+    }
+
+    public void doNothing1() {
+        for (int i = 0; i < 10; i++) {
+            int result = i + i;
+            result = result;
+        }
+    }
+
+    public void doNothing2() {
+        for (int i = 0; i < 10; i++) {
+            int result = i - i;
+            result = result;
+        }
+    }
+
+    public void doNothing3() {
+        for (int i = 0; i < 10; i++) {
+            int result = i / 1;
+            result = result;
+        }
+    }
+
+    public void doNothing4() {
+        for (int i = 0; i < 10; i++) {
+            int result = i % 1;
+            result = result;
+        }
+    }
+
+    public void doNothing5() {
+        for (int i = 0; i < 10; i++) {
+            int result = i * 2;
+            result = result;
+        }
+    }
+
+    public void doNothing6() {
+        for (int i = 0; i < 10; i++) {
+            int result = i * 3;
+            result = result;
+        }
+    }
+
+    public void doNothing7() {
+        for (int i = 0; i < 10; i++) {
+            int result = i * 4;
+            result = result;
+        }
+    }
+
+    public void doNothing8() {
+        for (int i = 0; i < 10; i++) {
+            int result = i * 5;
+            result = result;
+        }
+    }
+
+    public void doNothing9() {
+        for (int i = 0; i < 10; i++) {
+            int result = i * 6;
+            result = result;
+        }
+    }
+
+    public void doNothing10() {
+        for (int i = 0; i < 10; i++) {
+            int result = i * 7;
+            result = result;
+        }
+    }
+
+    public void doNothing11() {
+        for (int i = 0; i < 10; i++) {
+            int result = i * 8;
+            result = result;
+        }
+    }
+
+    public void doNothing12() {
+        for (int i = 0; i < 10; i++) {
+            int result = i * 9;
+            result = result;
+        }
+    }
+
+    public void doNothing13() {
+        for (int i = 0; i < 10; i++) {
+            int result = i * 10;
+            result = result;
+        }
+    }
+
+    public void doNothing14() {
+        for (int i = 0; i < 10; i++) {
+            int result = i * 11;
+            result = result;
+        }
+    }
+
+    public void doNothing15() {
+        for (int i = 0; i < 10; i++) {
+            int result = i * 12;
+            result = result;
+        }
+    }
+
+    public void doNothing16() {
+        for (int i = 0; i < 10; i++) {
+            int result = i * 13;
+            result = result;
+        }
+    }
+
+    public void doNothing17() {
+        for (int i = 0; i < 10; i++) {
+            int result = i * 14;
+            result = result;
+        }
+    }
+
+    public void doNothing18() {
+        for (int i = 0; i < 10; i++) {
+            int result = i * 15;
+            result = result;
+        }
+    }
+
+    public void doNothing19() {
+        for (int i = 0; i < 10; i++) {
+            int result = i * 16;
+            result = result;
+        }
+    }
+
+    public void doNothing20() {
+        for (int i = 0; i < 10; i++) {
+            int result = i * 17;
+            result = result;
+        }
+    }
+
+    public void doNothing21() {
+        for (int i = 0; i < 10; i++) {
+            int result = i * 18;
+            result = result;
+        }
+    }
+
+    public void doNothing22() {
+        for (int i = 0; i < 10; i++) {
+            int result = i * 19;
+            result = result;
+        }
+    }
+
+    public void doNothing23() {
+        for (int i = 0; i < 10; i++) {
+            int result = i * 20;
+            result = result;
+        }
+    }
+
+    public void doNothing24() {
+        for (int i = 0; i < 10; i++) {
+            int result = i * 21;
+            result = result;
+        }
+    }
+
+    public void doNothing25() {
+        for (int i = 0; i < 10; i++) {
+            int result = i * 22;
+            result = result;
+        }
+    }
+
+    public void doNothing26() {
+        for (int i = 0; i < 10; i++) {
+            int result = i * 23;
+            result = result;
+        }
+    }
+
+    public void doNothing27() {
+        for (int i = 0; i < 10; i++) {
+            int result = i * 24;
+            result = result;
+        }
+    }
+
+    public void doNothing28() {
+        for (int i = 0; i < 10; i++) {
+            int result = i * 25;
+            result = result;
+        }
+    }
+
+    public void doNothing29() {
+        for (int i = 0; i < 10; i++) {
+            int result = i * 26;
+            result = result;
+        }
+    }
+
+    public void doNothing30() {
+        for (int i = 0; i < 10; i++) {
+            int result = i * 27;
+            result = result;
+        }
+    }
+
+    public void doNothing31() {
+        for (int i = 0; i < 10; i++) {
+            int result = i * 28;
+            result = result;
+        }
+    }
+
+    public void doNothing32() {
+        for (int i = 0; i < 10; i++) {
+            int result = i * 29;
+            result = result;
+        }
+    }
+
+    public void doNothing33() {
+        for (int i = 0; i < 10; i++) {
+            int result = i * 30;
+            result = result;
+        }
+    }
+
+    public void doNothing34() {
+        for (int i = 0; i < 10; i++) {
+            int result = i * 31;
+            result = result;
+        }
+    }
+
+    public void doNothing35() {
+        for (int i = 0; i < 10; i++) {
+            int result = i * 32;
+            result = result;
+        }
+    }
+
+    public void doNothing36() {
+        for (int i = 0; i < 10; i++) {
+            int result = i * 33;
+            result = result;
+        }
+    }
+
+    public void doNothing37() {
+        for (int i = 0; i < 10; i++) {
+            int result = i * 34;
+            result = result;
+        }
+    }
+
+    public void doNothing38() {
+        for (int i = 0; i < 10; i++) {
+            int result = i * 35;
+            result = result;
+        }
+    }
+
+    public void doNothing39() {
+        for (int i = 0; i < 10; i++) {
+            int result = i * 36;
+            result = result;
+        }
+    }
+
+    public void doNothing40() {
+        for (int i = 0; i < 10; i++) {
+            int result = i * 37;
+            result = result;
+        }
+    }
+
+    public void doNothing41() {
+        for (int i = 0; i < 10; i++) {
+            int result = i * 38;
+            result = result;
+        }
+    }
+
+    public void doNothing42() {
+        for (int i = 0; i < 10; i++) {
+            int result = i * 39;
+            result = result;
+        }
+    }
+
+    public void doNothing43() {
+        for (int i = 0; i < 10; i++) {
+            int result = i * 40;
+            result = result;
+        }
+    }
+
+    public void doNothing44() {
+        for (int i = 0; i < 10; i++) {
+            int result = i * 41;
+            result = result;
+        }
+    }
+
+    public void doNothing45() {
+        for (int i = 0; i < 10; i++) {
+            int result = i * 42;
+            result = result;
+        }
+    }
+
+    public void doNothing46() {
+        for (int i = 0; i < 10; i++) {
+            int result = i * 43;
+            result = result;
+        }
+    }
+
+    public void doNothing47() {
+        for (int i = 0; i < 10; i++) {
+            int result = i * 44;
+            result = result;
+        }
+    }
+
+    public void doNothing48() {
+        for (int i = 0; i < 10; i++) {
+            int result = i * 45;
+            result = result;
+        }
+    }
+
+    public void doNothing49() {
+        for (int i = 0; i < 10; i++) {
+            int result = i * 46;
+            result = result;
+        }
+    }
+
+    public void doNothing50() {
+        for (int i = 0; i < 10; i++) {
+            int result = i * 47;
+            result = result;
+        }
+    }
+}
+
